b1e60aa6aac352ea03e4ac5ad20d709c67f478f1
Zohaib Haider <zebihaider123@gmail.com>
2024-11-01 11:04:40 +0500

    changes done

--- acta-2022/src/modules/AdminComponents/components/FormNavigationButtons.jsx
+++ acta-2022/src/modules/AdminComponents/components/FormNavigationButtons.jsx
@@ -1,3 +1,5 @@
+import { current } from "@reduxjs/toolkit";
+
 const FormNavigationButtons = ({
   currentFormIndex,
   handleBack,
@@ -12,9 +14,9 @@ const FormNavigationButtons = ({
   if (currentFormIndex === 8) {
     // Index 8 corresponds to Form 9
     return (
-      <div className="flex flex-col w-full gap-y-4">
+      <div className="flex smd:flex-row flex-col smd:justify-between w-full gap-y-4">
         {/* Top buttons */}
-        <div className="flex flex-col gap-y-2 w-full">
+        <div className="flex smd:flex-row flex-col gap-x-2 gap-y-2 w-full smd:w-fit ">
           <button
             onClick={handleBack}
             disabled={currentFormIndex === 0}
@@ -38,12 +40,12 @@ const FormNavigationButtons = ({
 
         {/* Bottom buttons */}
         {isLastForm ? (
-          <div className="flex flex-col gap-y-2 w-full">
+          <div className="flex smd:flex-row gap-x-2 flex-col gap-y-2 smd:w-fit">
             <button
               type="button"
               onClick={() => handleApplicationStatus("rejected")}
               disabled={isSubmitting}
-              className="w-full px-4 py-2 font-semibold text-white bg-red-500 rounded-md hover:bg-red-700 disabled:opacity-50"
+              className=" px-4 py-2 font-semibold text-white bg-red-500 rounded-md hover:bg-red-700 disabled:opacity-50"
             >
               Reject Application
             </button>
@@ -51,7 +53,7 @@ const FormNavigationButtons = ({
               type="button"
               onClick={() => handleApplicationStatus("approved")}
               disabled={isSubmitting}
-              className="w-full px-4 py-2 font-semibold text-white bg-green-500 rounded-md hover:bg-green-700 disabled:opacity-50"
+              className=" px-4 py-2 font-semibold text-white bg-green-500 rounded-md hover:bg-green-700 disabled:opacity-50"
             >
               Approve Application
             </button>
@@ -60,7 +62,7 @@ const FormNavigationButtons = ({
           <button
             onClick={handleNext}
             disabled={!isNextEnabled()}
-            className={`w-full px-4 py-2 rounded ${
+            className={` px-4 py-2 rounded ${
               !isNextEnabled()
                 ? "bg-gray-300 cursor-not-allowed"
                 : "bg-blue-500 hover:bg-blue-600 text-white"
@@ -72,7 +74,69 @@ const FormNavigationButtons = ({
       </div>
     );
   }
+  if (currentFormIndex !== 8) {
+    return (
+      <div className="flex smd:flex-row flex-col smd:justify-between w-full gap-y-4">
+        {/* Top buttons */}
+        <div className="flex flex-col gap-y-2 w-full smd:w-fit ">
+          <button
+            onClick={handleBack}
+            disabled={currentFormIndex === 0}
+            className={`w-full px-4 py-2 rounded ${
+              currentFormIndex === 0
+                ? "bg-gray-300 cursor-not-allowed"
+                : "bg-blue-500 hover:bg-blue-600 text-white"
+            }`}
+          >
+            Back
+          </button>
+        </div>
+        <div className="flex flex-row gap-x-2">
+          <button
+            type="submit"
+            onClick={handleClick}
+            className="w-full px-4 py-2 font-semibold text-white bg-green-500 rounded-md hover:bg-green-700"
+          >
+            Save
+          </button>
 
+          {/* Bottom buttons */}
+          {isLastForm ? (
+            <div className="flex flex-col gap-y-2 smd:w-fit">
+              <button
+                type="button"
+                onClick={() => handleApplicationStatus("rejected")}
+                disabled={isSubmitting}
+                className=" px-4 py-2 font-semibold text-white bg-red-500 rounded-md hover:bg-red-700 disabled:opacity-50"
+              >
+                Reject Application
+              </button>
+              <button
+                type="button"
+                onClick={() => handleApplicationStatus("approved")}
+                disabled={isSubmitting}
+                className=" px-4 py-2 font-semibold text-white bg-green-500 rounded-md hover:bg-green-700 disabled:opacity-50"
+              >
+                Approve Application
+              </button>
+            </div>
+          ) : (
+            <button
+              onClick={handleNext}
+              disabled={!isNextEnabled()}
+              className={` px-4 py-2 rounded ${
+                !isNextEnabled()
+                  ? "bg-gray-300 cursor-not-allowed"
+                  : "bg-blue-500 hover:bg-blue-600 text-white"
+              }`}
+            >
+              Next
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
   // Default layout for all other forms
   return (
     <div className="flex flex-row justify-between items-center w-full gap-x-2 md:-mt-4">
@@ -92,48 +156,6 @@ const FormNavigationButtons = ({
       </div>
 
       {/* Right - Action Buttons */}
-      <div className="flex gap-x-2">
-        <button
-          type="submit"
-          onClick={handleClick}
-          className="px-4 py-2 font-semibold text-white bg-green-500 rounded-md hover:bg-green-700"
-        >
-          Save
-        </button>
-
-        {isLastForm ? (
-          <>
-            <button
-              type="button"
-              onClick={() => handleApplicationStatus("rejected")}
-              disabled={isSubmitting}
-              className="px-4 py-2 font-semibold text-white bg-red-500 rounded-md hover:bg-red-700 disabled:opacity-50"
-            >
-              Reject Application
-            </button>
-            <button
-              type="button"
-              onClick={() => handleApplicationStatus("approved")}
-              disabled={isSubmitting}
-              className="px-4 py-2 font-semibold text-white bg-green-500 rounded-md hover:bg-green-700 disabled:opacity-50"
-            >
-              Approve Application
-            </button>
-          </>
-        ) : (
-          <button
-            onClick={handleNext}
-            disabled={!isNextEnabled()}
-            className={`px-4 py-2 rounded ${
-              !isNextEnabled()
-                ? "bg-gray-300 cursor-not-allowed"
-                : "bg-blue-500 hover:bg-blue-600 text-white"
-            }`}
-          >
-            Next
-          </button>
-        )}
-      </div>
     </div>
   );
 };
